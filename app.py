53c33f7cc402206a73976265c07e75eafa295fd0
manasiwalawalkar744-hash <manasiwalawalkar744@gmail.com>
2026-08-28 00:02:36 -0700

    Updated code app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import datetime
-
 import streamlit as st
 import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import datetime
 
 st.header("Student Mental health Tracker application")
 
